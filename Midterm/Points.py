55cd841b3ade1888337ff8950b8bee40e3cdf50c
Richard Feng <94466733+MyTastyDurian@users.noreply.github.com>
2022-07-22 09:01:23 -0230

    Create Points.py

---
+++ Midterm/Points.py
@@ -0,0 +1,19 @@
+class PointTL: 
+    def __init__(self, x, y): 
+        self.x = x 
+        self.y = y 
+
+class PointBR: 
+    def __init__(self, x, y): 
+        self.x = x 
+        self.y = y 
+
+class PointT: 
+    def __init__(self, x, y): 
+        self.x = x 
+        self.y = y 
+
+class PointB: 
+    def __init__(self, x, y): 
+        self.x = x 
+        self.y = y 
