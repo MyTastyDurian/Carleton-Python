86ca591b60d89fb36ee71e758e7692d65e4441a5
Richard Feng <94466733+MyTastyDurian@users.noreply.github.com>
2022-07-22 09:00:49 -0230

    Create main.py

---
+++ Midterm/main.py
@@ -0,0 +1,12 @@
+from buildingHouse import *
+
+still_making_house = True
+
+screen = Screen()
+screen.screensize(10000, 10000)
+
+build = buildingHouse()
+
+build.drawingHouse()
+build.drawingWindows()
+input()
